894cfacdaa3cb2dabd7146365d9b49c5423fd20c
Tarik Villalobos <tarik.villalobos@gmail.com>
2026-02-16 18:01:35 -0300

    chore: primeiro commit para JG (app sem testes)

---
+++ open-tasks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  open-tasks
+//
+//  Created by Tarik Villalobos on 2/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ open-tasks/open_tasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  open_tasksApp.swift
+//  open-tasks
+//
+//  Created by Tarik Villalobos on 2/16/26.
+//
+
+import SwiftUI
+
+@main
+struct open_tasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
